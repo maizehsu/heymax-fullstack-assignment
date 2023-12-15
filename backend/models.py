b275d8fd450016988fb8c048078e97e8b24073f2
Mai Xu <maixu@Mais-MacBook-Pro.local>
2023-12-15 11:04:57 +0800

    Extend data models and routes

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,17 @@
 Item class represents a catalog item.
 """
 class Item:
-    def __init__(self, id, name, description, price):
+    def __init__(self, id, name, description, price, inventory_count, category):
         self.id = id
         self.name = name
         self.description = description
         self.price = price
+        self.inventory_count = inventory_count
+        self.category = category
+
+    def apply_discount(self, discount):
+        # TODO: Add logic to apply discount to the price
+        pass
 
 """
 User class represents a user.
@@ -16,13 +22,19 @@ class User:
         self.id = id
         self.username = username
         self.email = email
+        self.shopping_cart = ShoppingCart()
+        self.order_history = []
+
+    def add_order(self, order):
+        # TODO: Add logic to add an order to order history
+        pass
 
 """
 ShoppingCart class represents a shopping cart.
 """
 class ShoppingCart:
     def __init__(self):
-        self.items = {}  # Key: Item ID, Value: Quantity
+        self.items = {}  # Key: Item ID, Value: (Item, Quantity)
 
     def add_item(self, item_id, quantity):
         if item_id in self.items:
@@ -36,15 +48,43 @@ class ShoppingCart:
             if self.items[item_id] <= 0:
                 del self.items[item_id]
 
+    def calculate_total(self):
+        # TODO: Add logic to calculate the total cost of items in the cart
+        pass
+
+    def apply_discounts(self):
+        # TODO: Add logic to apply discounts to items in the cart
+        pass
+
+"""
+Order class represents an order.
+"""
+class Order:
+    def __init__(self, id, user_id, items, total, date):
+        self.id = id
+        self.user_id = user_id
+        self.items = items
+        self.total = total
+        self.date = date
+
+"""
+Review class represents a product review.
+"""
+class Review:
+    def __init__(self, id, user_id, item_id, rating, comment):
+        self.id = id
+        self.user_id = user_id
+        self.item_id = item_id
+        self.rating = rating
+        self.comment = comment
+
 # Mock database
 catalog = [
-    Item(1, "Item 1", "Description 1", 10.99),
-    Item(2, "Item 2", "Description 2", 12.99),
+    Item(1, "Item 1", "Description 1", 10.99, 10, "Category 1"),
+    Item(2, "Item 2", "Description 2", 12.99, 10, "Category 1"),
 ]
 users = [
     User(1, "user1", "user1@example.com"),
     User(2, "user2", "user2@example.com"),
 ]
 carts = {user.id: ShoppingCart() for user in users}
-
-
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-from .models import catalog, users, ShoppingCart, carts
+from flask import jsonify, request
+from .models import catalog, users, ShoppingCart, carts, Order, Review
 
 def configure_routes(app):
     @app.route('/catalog', methods=['GET'])
@@ -25,3 +25,40 @@ def configure_routes(app):
 
         carts[user_id].remove_item(item_id, quantity)
         return jsonify({'success': True})
+
+    @app.route('/user/create', methods=['POST'])
+    def create_user():
+        # TODO: Add logic to create a new user
+        pass
+
+    @app.route('/discount/apply', methods=['POST'])
+    def apply_discount():
+        # TODO: Add logic to apply discount
+        pass
+
+    @app.route('/inventory/update', methods=['POST'])
+    def update_inventory():
+        # TODO: Add logic to update inventory
+        pass
+
+    @app.route('/reviews/add', methods=['POST'])
+    def add_review():
+        # TODO: Add logic to add a product review
+        pass
+
+    @app.route('/reviews/<int:item_id>', methods=['GET'])
+    def get_reviews(item_id):
+        # TODO: Add logic to get reviews for a specific item
+        pass
+
+    @app.route('/search', methods=['GET'])
+    def search_products():
+        # TODO: Add logic for product search and filtering by category
+        query = request.args.get('query', '')
+        category = request.args.get('category', '')
+        pass
+
+    @app.route('/order/history/<int:user_id>', methods=['GET'])
+    def view_order_history(user_id):
+        # TODO: Add logic to view a user's order history
+        pass
